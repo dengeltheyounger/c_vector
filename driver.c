39ae352b24f96ce5431d912e82297fd5d8af987b
dengel <dengeltheyounger@protonmail.com>
2020-09-27 21:01:13 -0400

    Added extra testing for shrink

--- driver.c
+++ driver.c
@@ -106,6 +106,18 @@ int main(int argc, char *argv[]) {
 	currindex = vector->get_current_index(vector);
 	currsize = vector->get_current_size(vector);
 	maxsize = vector->get_max_size(vector);
+
+	printf("current index: %ld, current size: %ld, max size: %ld\n", currindex, currsize, maxsize);
+	
+	code = vector->shrink(vector);
+	
+	if (code != 0) {
+		fprintf(stderr, "Value of code: %s", STRINGIFY(code));
+	}
+	
+	currindex = vector->get_current_index(vector);
+	currsize = vector->get_current_size(vector);
+	maxsize = vector->get_max_size(vector);
 	
 	printf("current index: %ld, current size: %ld, max size: %ld\n", currindex, currsize, maxsize);
 	
